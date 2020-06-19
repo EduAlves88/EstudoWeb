f9c93bb84c88869a38920df5d309d79ff07e845a
Eduardo Alves <32710203+EduAlves88@users.noreply.github.com>
2020-06-19 12:56:21 -0300

    Update TimeRepositorio.cs
    Código do repositório comentado

--- WebApplication2/WebApplication2/Repositorio/TimeRepositorio.cs
+++ WebApplication2/WebApplication2/Repositorio/TimeRepositorio.cs
@@ -7,62 +7,56 @@ using System.Linq;
 using System.Web;
 using WebApplication2.Models;
 
-namespace WebApplication2.Repositorio
+//Repositório para a String de conexão
+namespace WebApplication2.Repositorio 
 {
     public class TimeRepositorio
     {
         private SqlConnection _con;
-
-        private void Connection()
+        private void Connection() // Objeto de conexão
         {
-            string connstr = ConfigurationManager.ConnectionStrings["stringConexao"].ToString();
-            _con = new SqlConnection(connstr);
+            string connstr = ConfigurationManager.ConnectionStrings["stringConexao"].ToString(); // Conversão para String do endereço no banco vindo do "stringConection" em web.config
+            _con = new SqlConnection(connstr); // Instância do novo objeto de conexão
         }
-
         private static ConnectionStringSettingsCollection GetConnectionStrings()
         {
             return ConfigurationManager.ConnectionStrings;
         }
 
-
         //Adicionar um time
-
-
-        public bool AdicionarTime(Times timeObj)
+        public bool AdicionarTime(Times timeObj) 
         {
             Connection();
 
             int i;
 
-            using (SqlCommand command = new SqlCommand("IncluirTime", _con))
+            using (SqlCommand command = new SqlCommand("IncluirTime", _con))// usa a conexão _con para acessar uma procedue feita no sql server
             {
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@Time", timeObj.Time);
+                command.CommandType = CommandType.StoredProcedure; // busca a procedure armazenada
+                command.Parameters.AddWithValue("@Time", timeObj.Time); // adiciona o id informado ao @TimeId e encontra o elemento
                 command.Parameters.AddWithValue("@Estado", timeObj.Estado);
                 command.Parameters.AddWithValue("@Cores", timeObj.Cores);
-                _con.Open();
-                i = command.ExecuteNonQuery();
+                _con.Open(); //aciona a abertura da conexão já tendo os dados
+                i = command.ExecuteNonQuery();  //Executa a instrução na conexão, retorna o número de linhas afetadas e armazena em i
             }
-
-            _con.Close();
-            return i >= 1;
+            _con.Close(); // fecha a conexão após a ação
+            return i >= 1; //return condicional caso a variável 'i' não seja null
         }
 
-
         //Obter todos os times
-        public List<Times> ObterTimes()
+        public List<Times> ObterTimes() 
         {
             Connection();
 
-            List<Times> timesList = new List<Times>();
+            List<Times> timesList = new List<Times>(); // instância de uma lista para aguardar as informações obtidas
 
-            using (SqlCommand command = new SqlCommand("ObterTimes", _con))
+            using (SqlCommand command = new SqlCommand("ObterTimes", _con)) // instância do SQL Command para obtenção dos dados
             {
-                command.CommandType = CommandType.StoredProcedure;
-                _con.Open();
-                SqlDataReader reader = command.ExecuteReader();
+                command.CommandType = CommandType.StoredProcedure; // busca a procedure armazenada
+                _con.Open();// abre a conexão 
+                SqlDataReader reader = command.ExecuteReader(); // executa o reader para obter os dados do banco
 
-                while (reader.Read())
+                while (reader.Read()) // loop do READ, até que não obtenha mais informações e retorne nulo
                 {
                     timesList.Add(new Times
                     {
@@ -70,60 +64,51 @@ namespace WebApplication2.Repositorio
                         Time = reader["Time"].ToString(),
                         Estado = reader["Estado"].ToString(),
                         Cores = reader["Cores"].ToString()
-                    });
+                    });// adiciona cada um dos elementos do banco à lista criada anteriormente
                 }
             }
-
-            _con.Close();
-
-            return timesList;
+            _con.Close(); // fecha a conexão sempre após o uso do banco
+            return timesList; // retorna ao final a lista de times informados um a um no laço while
         }
-
-        public bool AtualizarTime(Times timeObj)
+        
+        // Atualizar times
+        public bool AtualizarTime(Times timeObj) // entra na função com os dados do time a atualizar
         {
             Connection();
-
             int i;
-
-            using (SqlCommand command = new SqlCommand("AtualizarTime", _con))
+            using (SqlCommand command = new SqlCommand("AtualizarTime", _con))// instância do SQL Command para obtenção dos dados
             {
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@TimeId", timeObj.TimeId);
+                command.CommandType = CommandType.StoredProcedure;// busca a procedure armazenada
+                command.Parameters.AddWithValue("@TimeId", timeObj.TimeId);// adiciona o id informado ao @TimeId e encontra o elemento
                 command.Parameters.AddWithValue("@Time", timeObj.Time);
                 command.Parameters.AddWithValue("@Estado", timeObj.Estado);
                 command.Parameters.AddWithValue("@Cores", timeObj.Cores);
-                _con.Open();
-                i = command.ExecuteNonQuery();
+                _con.Open(); // abre a conexão do banco para atualizar
+                i = command.ExecuteNonQuery(); //Executa a instrução na conexão, retorna o número de linhas afetadas e armazena em i
             }
-
-            _con.Close();
-            return i >= 1;
+            _con.Close(); // fecha a conexão
+            return i >= 1; // retorna o valor obtido caso não seja null
         }
-
-
-        public bool ExcluirTime(int id)
+            
+        //Excluir Time
+        public bool ExcluirTime(int id)// entra na função com o id do time escolhido
         {
             Connection();
-
             int i;
-
-            using (SqlCommand command = new SqlCommand("ExcluirTimePorId", _con))
+            using (SqlCommand command = new SqlCommand("ExcluirTimePorId", _con))// instância do SQL Command para obtenção dos dados
             {
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@TimeId", id);
+                command.CommandType = CommandType.StoredProcedure;// busca a procedure armazenada
+                command.Parameters.AddWithValue("@TimeId", id);// adiciona o id informado ao @TimeId e encontra o elemento
 
-                _con.Open();
-                i = command.ExecuteNonQuery();
+                _con.Open();// Abre a Conexão
+                i = command.ExecuteNonQuery();//Executa a instrução na conexão, retorna o número de linhas afetadas e armazena em i
             }
 
-            _con.Close();
-
-
+            _con.Close();// Fecha a Conexão
             if (i >= 1)
             {
                 return true;
             }
-
             return false;
         }
     }
